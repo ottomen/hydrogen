d20c6da5f60ecc078d4d8da1634155e106965b94
Arthur Shevchenko <ashev4@softserveinc.com>
2021-11-23 16:13:17 +0200

    Add Hydrogen files

---
+++ shopify.config.js
@@ -0,0 +1,6 @@
+export default {
+  locale: 'en-us',
+  storeDomain: 'hydrogen-softserve.myshopify.com',
+  storefrontToken: '395765e5a184c1b5ddb084ffa3a52214',
+  graphqlApiVersion: 'unstable',
+};
